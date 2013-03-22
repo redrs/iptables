f3030d235a94b91fc7f08f9809e90f46f8081b37
redrs <redrs@riseup.net>
2013-03-23 11:29:23 +1300

    ip fix add

--- iptables_limit_dns_users.sh
+++ iptables_limit_dns_users.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 #
 # limit dns users/servers with iptables
-
 IPTABLES=/sbin/iptables
+
+# your ip
+SERVER_OUT_IP="1.1.1.1"
 
 # open dns servers
 DNSSERVER="208.67.222.222 208.67.220.220"
@@ -13,7 +15,7 @@ for dnsip in $DNSSERVER
 do
   for dnsuser in $DNSUSERS
   do
-   $IPTABLES -A OUTPUT -p udp -m udp -d $dnsip --dport 53 --sport 1024:65535 -s $SERVER_IP_TUNNEL \
+   $IPTABLES -A OUTPUT -p udp -m udp -d $dnsip --dport 53 --sport 1024:65535 -s $SERVER_OUT_IP \
    -m conntrack --ctstate NEW --match owner --uid-owner $dnsuser -j ACCEPT \
    -m comment --comment "DNS out $dnsuser "
   done
